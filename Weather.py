4b168dd57cfa810a3d61c1ce96f16ae08f1bcbcf
Orangemarza <87427780+Orangemarza@users.noreply.github.com>
2022-01-19 16:50:51 +1100

    define getweather

--- Weather.py
+++ Weather.py
@@ -1,3 +1,13 @@
 import tkinter as tk
 import requests
 import time
+
+canvas = tk.Tk()
+canvas.geometry("600x500")
+canvas.title("Weather App")
+
+f= ("poppins", 15, "bold")
+t= ("poppins", 35, "bold")
+
+text = tk.Entry(canvas, font = t)
+textfield.pack(pady = 20)
