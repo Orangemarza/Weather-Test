7535ede9bed364acfb84afa4fc277326470bde68
Orangemarza <87427780+Orangemarza@users.noreply.github.com>
2022-01-19 20:46:14 +1100

    Weatherman

--- Weather.py
+++ Weather.py
@@ -25,7 +25,7 @@ def getWeather(canvas):
 
 canvas = tk.Tk()
 canvas.geometry("600x500")
-canvas.title("Weather Man")
+canvas.title("Weather Dude")
 f= ("poppins", 15, "bold")
 t= ("poppins", 35, "bold")
 
